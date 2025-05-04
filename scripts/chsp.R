7d0aa0180e2862c298a429b85d4d9fa8ff346c8c
tomdavidoff <thomas.davidoff@sauder.ubc.ca>
2025-05-03 22:17:15 -0700

    restart text

--- scripts/chsp.R
+++ scripts/chsp.R
@@ -4,16 +4,26 @@ library(fixest)
 
 df <- fread("data/raw/chsp2022.csv")
 print(summary(df))
-for (p in df[,`Period of construction`]) {
-	print(p)
-	for (l in df[,GEO]) {
-		for (k in unique(df[,`Residency participation`])) {
-			print(c(substring(p,1,4),substring(l,1,8),k,df[`Period of construction`==p & GEO==l & `Residency participation`==k,VALUE]))
+
+vv <- df[GEO=="Vancouver, Census metropolitan area (CMA)",log(VALUE),by=c("Residency participation","Period of construction")]
+vvg <- df[, log(VALUE),by=c("Residency participation","Period of construction","GEO")]
+# all condos: 830K/710K
+print(log(830)-log(710))
+print(summary(vv))
+print(vv)
+print(vvg)
+print(summary(vvg))
+vvg <- dcast(vvg, GEO+ `Period of construction` ~ `Residency participation` , value.var="V1")
+print(summary(vvg[,`Resident owners only`-`Non-resident participation`]))
+print(vvg)
+
+if (2<1) {
+	for (p in df[,`Period of construction`]) {
+		print(p)
+		for (l in df[,GEO]) {
+			for (k in unique(df[,`Residency participation`])) {
+				print(c(substring(p,1,4),substring(l,1,8),k,df[`Period of construction`==p & GEO==l & `Residency participation`==k,VALUE]))
+			}
 		}
 	}
 }
-vd <- mean(df[GEO=="Vancouver, Census metropolitan area (CMA)" & `Residency participation`=="Resident owners only",log(VALUE)])
-vf <- mean(df[GEO=="Vancouver, Census metropolitan area (CMA)" & `Residency participation`=="Non-resident participation",log(VALUE)])
-print(c(vd,vf))
-print(vd-vf)
-print(mean(df[`Residency participation`=="Resident owners only",log(VALUE),by=c("Period of construction","GEO")],na.rm=TRUE) - mean(df[`Residency participation`=="Non-resident participation",log(VALUE),by=c("Period of construction","GEO")],na.rm=TRUE))
--- scripts/heterogeneity.R
+++ scripts/heterogeneity.R
@@ -7,19 +7,52 @@ library(data.table)
 library(ggplot2)
 library(fixest)
 
-# read in mls.R from foreignTrend
-dsales <- fread("~/onedrive/foreignTrend/data/derived/mls.csv")
-dsales[, date := as.Date(`Sold Date`, format = "%m/%d/%Y")]
-dsales <- dsales[!is.na(date)]
-dsales[, price := as.numeric(gsub("\\$", "", gsub(",", "", `Price`)))]
-# convert to 1/30/2022 style to date 
+MADESALES <- 1
+if (MADESALES == 0) {
+	# read in mls.R from source
 
-# convert price from $1,000,482 to numeric
-for (t in c("Apartment/Condo", "HOUSE")) {
-	dst <- dsales[TypeDwel==t]
-	print(quantile(dst[date>as.Date("2015-01-01") & date<as.Date("2015-12-31"),price],probs=c(.1,.25,.5,.75,.9,.95,.975,.99)))
-	print(quantile(dst[date>as.Date("2016-02-01") & date<as.Date("2016-04-01"),price],probs=c(.1,.25,.5,.75,.9,.95,.975,.99)))
-	print(quantile(dst[date>as.Date("2016-04-01") & date<as.Date("2016-08-01"),price],probs=c(.1,.25,.5,.75,.9,.95,.975,.99)))
-	print(quantile(dst[date>as.Date("2016-08-15") & date<as.Date("2016-11-15"),price],probs=c(.1,.25,.5,.75,.9,.95,.975,.99)))
-	print(quantile(dst[date>as.Date("2017-01-01") & date<as.Date("2017-12-31"),price],probs=c(.1,.25,.5,.75,.9,.95,.975,.99)))
+	df <- fread("~/docs/data/bca/data_advice_REVD24_20240331/bca_folio_sales_20240331_REVD24.csv",select=c("JURISDICTION_CODE","CONVEYANCE_DATE","CONVEYANCE_PRICE","CONVEYANCE_TYPE_DESCRIPTION","FOLIO_ID","ROLL_NUMBER"))
+	print(summary(df))
+
+	da <- fread("~/docs/data/bca/data_advice_REVD24_20240331/bca_folio_addresses_20240331_REVD24.csv",select=c("FOLIO_ID","UNIT_NUMBER","STREET_NUMBER","STREET_DIRECTION_PREFIX","STREET_NAME","STREET_TYPE","STREET_DIRECTION_SUFFIX","CITY","POSTAL_CODE","PROVINCE","JURISDICTION_CODE"))
+	da[,building:=paste(STREET_NUMBER,STREET_DIRECTION_PREFIX,STREET_NAME,JURISDICTION_CODE)]
+	da[,buildingN:=.N,by=c("building")]
+	df <- merge(df,da,by=c("FOLIO_ID","JURISDICTION_CODE"))
+
+	dd <- fread("~/docs/data/bca/data_advice_REVD24_20240331/bca_folio_descriptions_20240331_REVD24.csv",select=c("FOLIO_ID","ACTUAL_USE_DESCRIPTION","JURISDICTION_CODE","REGIONAL_DISTRICT"))
+
+	df <- merge(df,dd,by=c("FOLIO_ID","JURISDICTION_CODE"))
+	fwrite(df,"data/derived/sales.csv")
 }
+df <- fread("data/derived/sales.csv")
+
+print(summary(df))
+print(table(df[,REGIONAL_DISTRICT]))
+df <- df[REGIONAL_DISTRICT=="Metro Vancouver"]
+print(table(df[,ACTUAL_USE_DESCRIPTION]))
+df <- df[ACTUAL_USE_DESCRIPTION=="Strata-Lot Residence (Condominium)"]
+print(table(df[,CONVEYANCE_TYPE_DESCRIPTION]))
+df <- df[CONVEYANCE_TYPE_DESCRIPTION=="Improved Single Property Transaction"]
+print(df[1:10,CONVEYANCE_DATE])
+df[,year:=as.numeric(substring(CONVEYANCE_DATE,1,4))]
+df[,month:=as.numeric(substring(CONVEYANCE_DATE,5,6))]
+df[,yearQuarter:=year+(ceiling((month)/3)/4)]
+df <- df[year>=2010]
+df <- df[!is.na(CONVEYANCE_PRICE)]
+df <- df[CONVEYANCE_PRICE>quantile(CONVEYANCE_PRICE,0.0001) & CONVEYANCE_PRICE<quantile(CONVEYANCE_PRICE,0.9999)]
+df[,bMean:=mean(log(CONVEYANCE_PRICE),na.rm=TRUE),by=c("building","yearQuarter")]
+dv <- df[, .(all=var(log(CONVEYANCE_PRICE)),between=var(bMean),within=var(log(CONVEYANCE_PRICE)-bMean)),by=yearQuarter]
+dv <- melt(dv, id.vars="yearQuarter", variable.name="type", value.name="var")
+print(dv)
+ggplot(dv, aes(x=yearQuarter,y=var,group=type,color=type)) +
+	geom_line() +
+	geom_point() +
+	ggtitle("Variance Decomposition of Log Price") +
+	xlab("Year") +
+	ylab("Variance") +
+	geom_vline(xintercept=2016.75,linetype="dashed",color="red") +
+	theme(legend.position="bottom")
+ggsave("text/variance_decomposition.png")
+
+q("no")
+
